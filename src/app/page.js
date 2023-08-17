cfa408dad88dc3f4c20f5f8eb280ab90cc081fb1
winaldomanurung <winaldo.code@gmail.com>
2023-08-17 15:31:26 +0700

    first config

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,123 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { Container, Row, Col, Media, Image, Card } from "react-bootstrap";
+import BlogNavbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <Container>
+      <BlogNavbar />
+      <div className="blog-detail-page">
+        <Row>
+          <Col md="8">
+            {/* AUTHOR INTRO STARTS */}
+            <Media className="mb-4 admin-intro">
+              <Image
+                roundedCircle
+                width={64}
+                height={64}
+                className="mr-3"
+                src="https://avatars1.githubusercontent.com/u/9482724?s=460&u=69a6acab13fd5547a4e316e496b573271077147f&v=4"
+                alt="Generic placeholder"
+              />
+              <Media.Body>
+                <h5 className="font-weight-bold mb-0">Hello Friends,</h5>
+                <p className="welcome-text">
+                  My name is Filip Jerga and I am an experienced software
+                  engineer and freelance developer. and this is my blog page.
+                </p>
+              </Media.Body>
+            </Media>
+            {/* AUTHOR INTRO ENDS */}
+          </Col>
+        </Row>
+        <hr />
+        {/* className from props */}
+        <div className={`page-wrapper`}>
+          <Row className="mb-5">
+            <Col md="10">
+              {/* CardListItem STARTS */}
+              <Card className={`fj-card fj-card-list`}>
+                <div className="card-body-wrapper">
+                  <Card.Header className="d-flex flex-row">
+                    <img
+                      src={"https://via.placeholder.com/150"}
+                      className="rounded-circle mr-3"
+                      height="50px"
+                      width="50px"
+                      alt="avatar"
+                    />
+                    <div>
+                      <Card.Title className="font-weight-bold mb-1">
+                        Placeholder Author
+                      </Card.Title>
+                      <Card.Text className="card-date">
+                        Placeholder Date
+                      </Card.Text>
+                    </div>
+                  </Card.Header>
+                  <Card.Body>
+                    <Card.Title className="card-main-title">
+                      Placeholder Title
+                    </Card.Title>
+                    <Card.Text>Placehodler Subtitle</Card.Text>
+                  </Card.Body>
+                </div>
+                <a href="#" className="card-button">
+                  Read More
+                </a>
+              </Card>
+              {/* CardListItem ENDS */}
+            </Col>
+
+            <Col md="4">
+              <Card className={`fj-card`}>
+                <div className="card-body-wrapper">
+                  <Card.Header className="d-flex flex-row">
+                    <img
+                      src={"https://via.placeholder.com/150"}
+                      className="rounded-circle mr-3"
+                      height="50px"
+                      width="50px"
+                      alt="avatar"
+                    />
+                    <div>
+                      <Card.Title className="font-weight-bold mb-1">
+                        Placeholder Author
+                      </Card.Title>
+                      <Card.Text className="card-date">
+                        Placeholder Date
+                      </Card.Text>
+                    </div>
+                  </Card.Header>
+                  <div className="view overlay">
+                    <Card.Img
+                      src="https://via.placeholder.com/250"
+                      alt="Card image cap"
+                    />
+                  </div>
+                  <Card.Body>
+                    <Card.Title className="card-main-title">
+                      Placeholder Title
+                    </Card.Title>
+                    <Card.Text>Placehodler Subtitle</Card.Text>
+                  </Card.Body>
+                </div>
+                <a className="card-button">Read More</a>
+              </Card>
+            </Col>
+          </Row>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <footer className="page-footer">
+        <div>
+          <a href="#">courses</a>
+          {" | "}
+          <a href="#">github</a>
+          {" | "}
+          <a href="#">facebook</a>
+        </div>
+      </footer>
+    </Container>
+  );
 }
